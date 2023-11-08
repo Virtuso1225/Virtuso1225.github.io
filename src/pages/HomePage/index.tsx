d056bd7efde6121d1ad5dde5a67a6f220a87eb3c
hypnotes <hyp0notes@gmail.com>
2023-11-08 17:08:27 +0900

    ftr: home page (not responsive yet)

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,49 +1,78 @@
-import { Box, Grid, Typography, useTheme } from '@mui/material'
+import { Box, Container, Grid, Typography, useTheme } from '@mui/material'
 import { Helmet } from 'react-helmet-async'
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import { Link } from 'react-router-dom'
+import { LinkTypography } from '@src/components/LinkBox'
 
 const HomePage = () => {
   const theme = useTheme()
+  const isMobile = theme.breakpoints.down('md')
+
   return (
     <>
       <Helmet>
         <title>AI TECH DAY 2023</title>
       </Helmet>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          // paddingY: 10,
-          paddingX: theme.spacing(6),
-          borderColor: 'white',
-          height: '70vh'
-        }}
-      >
-        <Grid container rowGap={10}>
-          <Grid item sm={12} md={7} lg={7}>
-            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-              <Typography variant="h1" fontWeight="700">
-                정보대학 인공지능
-              </Typography>
-              <Typography variant="h1" fontWeight="700">
-                학회 컨퍼런스
+      <Grid container spacing={10} alignItems="center" justifyContent="center" width={'100%'} height={'100%'}>
+        <Grid item sm={12} md={6} lg={6} mt={8}>
+          <Box>
+            <Box display={'flex'} mt={1} alignItems={'baseline'}>
+              <Typography variant="h6">고려대학교 ICT 명품인재양성사업단</Typography>
+              <Typography variant="h5" fontWeight={'bold'}>
+                &nbsp;2023
               </Typography>
             </Box>
-          </Grid>
-          <Grid item sm={12} md={5} lg={5}>
-            <Box>
-              <Typography variant="body1">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut sapien at massa varius iaculis.
-                Curabitur vel bibendum justo. Nullam euismod, ante ac vulputate bibendum, justo nisi vulputate tellus,
-                vel accumsan libero ligula a tellus. In hac habitasse platea dictumst. Suspendisse cursus, arcu in
-                aliquam sollicitudin, orci quam ullamcorper urna, in auctor libero sapien non libero. Nullam interdum
-                risus non odio posuere, non vestibulum tortor vulputate. Sed efficitur velit vitae eros pellentesque,
-                vel hendrerit quam mattis.{' '}
-              </Typography>
-            </Box>
-          </Grid>
+            <Typography variant={isMobile ? 'h2' : 'h1'} sx={{ fontWeight: 900, marginBottom: theme.spacing(2) }}>
+              AI TECH DAY
+            </Typography>
+            <Link to={'/program'} style={{ textDecoration: 'none' }}>
+              <LinkTypography>
+                <CalendarTodayIcon color="inherit" fontSize="small" />
+                <Typography variant="body1" ml={1} fontWeight={'regular'} color="inherit">
+                  2023. 11. 22 (수) 10:00 ~ 18:10
+                </Typography>
+              </LinkTypography>
+            </Link>
+            <Link to={'/location'} style={{ textDecoration: 'none' }}>
+              <LinkTypography>
+                <LocationOnIcon color="inherit" fontSize="small" />
+                <Typography variant="body1" ml={1} fontWeight={'regular'} color="inherit">
+                  고려대학교 하나스퀘어 강당 (지하 1층)
+                </Typography>
+              </LinkTypography>
+            </Link>
+          </Box>
         </Grid>
-      </Box>
+        <Grid item sm={12} md={6} lg={6} mt={8}>
+          <Box>
+            <Typography variant="subtitle1" fontWeight="bold" color={theme.palette.text.secondary}>
+              초청강연
+            </Typography>
+            <Typography variant="caption" mt={1}>
+              네이버클라우드 &nbsp;/ &nbsp; LG AI Research &nbsp;/ &nbsp; SKT / &nbsp; 삼성SDS &nbsp;/ &nbsp; 한화시스템
+              &nbsp;/ &nbsp; 카카오브레인
+            </Typography>
+          </Box>
+          <Box mt={4}>
+            <Typography variant="subtitle1" fontWeight="bold" color={theme.palette.text.secondary}>
+              연구성과 공유
+            </Typography>
+            <Typography variant="caption" mt={1}>
+              산업체 연구 소개 &nbsp;/ &nbsp; 포스터,데모 시상
+            </Typography>
+          </Box>
+          <Box mt={4}>
+            <Typography variant="subtitle1" fontWeight="bold" color={theme.palette.text.secondary}>
+              5개 기업 Recruiting
+            </Typography>
+            <Typography variant="caption" mt={1}>
+              삼성리서치 &nbsp;/ &nbsp; 삼성전기 &nbsp;/ &nbsp; 한화시스템 &nbsp;/ &nbsp; 퓨리오사 AI &nbsp;/ &nbsp;
+              VIVE STUDIOS
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
     </>
   )
 }
--- src/pages/ProgramPage/index.tsx
+++ src/pages/ProgramPage/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import ScheduleGrid from './ScheduleGrid'
 import { LinkTypography } from '@src/components/LinkBox'
 import { Link } from 'react-router-dom'
@@ -6,7 +6,6 @@ import PageContainer from '@src/components/PageContainer'
 import PageTitle from '@src/components/PageContainer/PageTitle'
 
 const ProgramPage = () => {
-
   return (
     <PageContainer>
       <Grid container sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
