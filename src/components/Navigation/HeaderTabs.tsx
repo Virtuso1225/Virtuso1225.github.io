1056f2b127c4691cd4319295b57f1a5d27ec55a6
Virtuso1225 <dominico1225@gmail.com>
2023-11-08 14:32:35 +0900

    mod: remove not used import

--- src/components/Navigation/HeaderTabs.tsx
+++ src/components/Navigation/HeaderTabs.tsx
@@ -1,8 +1,6 @@
 import { Box, Tab, Tabs } from '@mui/material'
-import NavigationPaths from '@src/data/navigation'
-import { useState } from 'react'
+
 import { useTranslation } from 'react-i18next'
-import { useLocation, useNavigate } from 'react-router-dom'
 
 interface HeaderTabsProps {
   currentTab: number
@@ -10,9 +8,6 @@ interface HeaderTabsProps {
 }
 const HeaderTabs = ({ currentTab, handleChange }: HeaderTabsProps) => {
   const { t } = useTranslation()
-
-  const location = useLocation()
-  const isHome = location.pathname === '/'
 
   return (
     <Box sx={{ marginLeft: 'auto' }}>
