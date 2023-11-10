5f410f8e0d0cb183ccb9488ff52982ad8d48191f
Virtuso1225 <dominico1225@gmail.com>
2023-11-10 14:57:50 +0900

    ftr: map link

--- src/pages/LocationPage/KakaoMap.tsx
+++ src/pages/LocationPage/KakaoMap.tsx
@@ -24,7 +24,22 @@ const KakaoMap = () => {
     const marker = new kakao.maps.Marker({
       position: markerPosition
     })
+    // const linePath = [
+    //   new kakao.maps.LatLng(37.5843, 127.02555),
+    //   new kakao.maps.LatLng(37.5853, 127.0275),
+    //   new kakao.maps.LatLng(37.5862, 127.0274),
+    //   new kakao.maps.LatLng(37.586192, 127.0278576)
+    // ]
 
+    // // 지도에 표시할 선을 생성합니다
+    // const polyline = new kakao.maps.Polyline({
+    //   path: linePath, // 선을 구성하는 좌표배열 입니다
+    //   strokeWeight: 5, // 선의 두께 입니다
+    //   strokeColor: '#FFAE00', // 선의 색깔입니다
+    //   strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    //   strokeStyle: 'solid' // 선의 스타일입니다
+    // })
+    // polyline.setMap(map)
     marker.setMap(map)
   }, [])
 
--- src/pages/LocationPage/index.tsx
+++ src/pages/LocationPage/index.tsx
@@ -1,11 +1,13 @@
-import { Box, Divider, Grid, Typography, useTheme } from '@mui/material'
+import { Box, Button, Divider, Grid, Typography, useTheme } from '@mui/material'
 import PageContainer from '@src/components/PageContainer'
 import PageTitle from '@src/components/PageContainer/PageTitle'
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 import KakaoMap from './KakaoMap'
-
+import LinkIcon from '@mui/icons-material/Link'
 const LOCATION = '서울 성북구 고려대로22길 6-7'
 const SPECIFIC_LOCATION = '고려대학교 하나스퀘어 (지하1층)'
+const MAP_ROUTE_URL =
+  'https://map.kakao.com/link/from/%EC%95%88%EC%95%94%EC%97%AD%206%ED%98%B8%EC%84%A0,%2037.5863,%20127.0293/to/%EA%B3%A0%EB%A0%A4%EB%8C%80%ED%95%99%EA%B5%90%ED%95%98%EB%82%98%EC%8A%A4%ED%80%98%EC%96%B4,37.5846168,%20127.0253533'
 
 const LocationPage = () => {
   const theme = useTheme()
@@ -61,6 +63,22 @@ const LocationPage = () => {
             </Typography>
             <Divider sx={{ height: 3, width: '12%', backgroundColor: '#02306C', ml: theme.spacing(2) }} />
           </Box>
+          <Box
+            component={Button}
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: 1
+            }}
+            onClick={() => window.open(MAP_ROUTE_URL, '_blank')}
+          >
+            <LinkIcon color="secondary" sx={{ width: 21, height: 21 }} />
+            <Typography variant="body2" fontWeight={800}>
+              지도 바로가기
+            </Typography>
+          </Box>
           <KakaoMap />
           {/* <Box sx={{ padding: theme.spacing(1) }}>
             <img src="/assets/map.svg" width="100%" height="100%" />
@@ -82,7 +100,7 @@ const LocationPage = () => {
             <Divider sx={{ height: 3, width: '12%', backgroundColor: '#02306C', ml: theme.spacing(2) }} />
           </Box>
           <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <img src="/assets/blueprintNew.png" width="70%" />
+            <img src="/assets/blueprintNew.png" width="50%" />
           </Box>
         </Grid>
       </Grid>
