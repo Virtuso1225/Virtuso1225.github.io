e6a3a103d4357d5c63dfe686de573ab6af3aa1ae
Virtuso1225 <dominico1225@gmail.com>
2023-11-11 21:37:02 +0900

    mod: 4K screen View

--- src/pages/IntroductionPage/LectureContainer.tsx
+++ src/pages/IntroductionPage/LectureContainer.tsx
@@ -3,25 +3,26 @@ import type { INVITED_LECTURER } from '@src/data/introduction'
 
 const LectureContainer = ({ lecturer }: { lecturer: INVITED_LECTURER }) => {
   const theme = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.up('md'))
+  const isMd = useMediaQuery(theme.breakpoints.up('md'))
   const isLg = useMediaQuery(theme.breakpoints.up('lg'))
-
+  const is4K = useMediaQuery('(min-width: 2560px)')
+  const WIDTH = is4K ? '32%' : isLg ? '48%' : '85%'
   return (
     <Box
       sx={{
         display: 'flex',
         flexDirection: 'column',
-        padding: matches ? theme.spacing(3) : '',
+        padding: isMd ? theme.spacing(3) : '',
         mt: theme.spacing(8),
-        width: isLg ? '48%' : '80%'
+        width: WIDTH
       }}
     >
       <Grid container sx={{ display: 'flex', flexDirection: 'column' }}>
         <Grid container item sx={{ display: 'flex', flexDirection: 'row', marginBottom: 1 }}>
-          <Grid item lg={4} sm={12}>
+          <Grid item lg={6} md={6} sm={12}>
             <img src={lecturer.profile} width={180} height={180} />
           </Grid>
-          <Grid item lg={8} sm={12}>
+          <Grid item lg={6} md={6} sm={12}>
             <Box sx={{ display: 'flex', flexDirection: 'column' }}>
               <Box
                 sx={{
