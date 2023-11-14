949342cc0b73ae90f0f3ea1fdbc54471798ef30d
Virtuso1225 <dominico1225@gmail.com>
2023-11-14 19:09:25 +0900

    hotfix- refresh 404 error

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <HelmetProvider>
         <App />
       </HelmetProvider>
